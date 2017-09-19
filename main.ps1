e394e766cc908fc462b8dbc474063e59e34bb59b
slepoj-nosorog <32089614+slepoj-nosorog@users.noreply.github.com>
2017-09-19 16:06:51 +0300

    Create level class
    added class constructor and method

--- main.ps1
+++ main.ps1
@@ -9,7 +9,86 @@ Function writetext
 {
   Param ([int]$x, [int]$y, [string]$text)
 	
+	[Console]::SetCursorPosition($x, $y)if ($(Get-Host).Name.Contains("ISE")){
+
+    start-process PowerShell.exe -argument $MyInvocation.MyCommand.Definition
+    Return
+
+}
+
+Function writetext($x, $y , $symbol){
+	
 	[Console]::SetCursorPosition($x, $y)
+	[Console]::Write($symbol)
+    [Console]::CursorVisible = $false
+
+}
+
+Class Level{
+    
+    [System.Array]$map
+    [int]$width
+    [int]$height
+
+    # Class constructor
+    Level ([int]$width, [int]$height, [char]$char){
+        
+        $this.width = $width
+        $this.height = $height
+
+        $this.map = New-Object "object[,]" $width,$height
+
+        for ($x = 0; $x -lt $width; $x++) {
+            for ($y = 0; $y -lt $height; $y++) {
+                $this.map[$x,$y] = $char
+            }
+        }
+    }
+
+    [char]Write([int]$x,[int]$y){
+        Return $this.map[$x,$y]
+    }
+}
+
+$width = 20
+$height = 20
+
+$map = [Level]::new($width,$height,'.')
+
+function newPlayer()
+{
+	$player = New-Object PSObject
+	$player | Add-Member -type Noteproperty -Name x -Value 3
+	$player | Add-Member -type Noteproperty -Name y -Value 3
+	$player | Add-Member -type Noteproperty -Name char -Value "@"
+
+	return $player
+}
+
+#create the player object
+$player = newPlayer
+
+# main loop
+do
+{
+	[Console]::Clear()
+	for($i=1; $i -le $width-1; $i++)
+	{
+		for($j=1; $j -le $height-1; $j++)
+		{
+			writetext $i $j $map.Write($i,$j)
+		}
+	}
+	writetext $player.x $player.y $player.char
+
+	$input = [Console]::ReadKey(("NoEcho"))
+	if ($input.key -eq "RightArrow"){ $player.x = $player.x + 1 }
+	if ($input.key -eq "LeftArrow"){ $player.x = $player.x - 1 }
+	if ($input.key -eq "DownArrow"){ $player.y = $player.y + 1 }
+	if ($input.key -eq "UpArrow"){ $player.y = $player.y - 1 }
+} while ($input.keychar -ne "q")
+
+
 	[Console]::Write($text)
     [Console]::CursorVisible = $false
 }
