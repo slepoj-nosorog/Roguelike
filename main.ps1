2317045595538b8fb9c17010cdcc4c162fc51b54
slepoj-nosorog <32089614+slepoj-nosorog@users.noreply.github.com>
2017-09-21 17:58:40 +0300

    ++

--- main.ps1
+++ main.ps1
@@ -15,15 +15,27 @@ function pixel(){
 
 }
 
-[Hashtable]$DisplaySize = @{ width = 70; height = 50}
+[Hashtable]$DisplaySize = @{ width = 70; height = 25}
 [Console]::CursorVisible = $false
 [Console]::Title = "!Roguelike Zombie!"
 [Console]::BufferWidth = $DisplaySize.width
 [Console]::BufferHeight = $DisplaySize.height
 [Console]::WindowWidth = $DisplaySize.width
-[Console]::WindowHeight = $DisplaySize.height
+[Console]::WindowHeight = $DisplaySize.height + 20
+$levelWidth = $DisplaySize.width / 2
+$levelHeight = $DisplaySize.height
 
 Class Level{
+    
+    <#
+        На данный момент клас это матрица.
+        Если каджый обьект хранит свое место расположение то 
+        класс Level Это просто место хранение всех обьектов на уровне
+        Список.
+
+        Возможно стоит переписать его под список.
+        Или ваывести в переменную для сохранения герератора уровней
+    #>
 
     [System.Array]$map
 
@@ -32,13 +44,13 @@ Class Level{
         for ($x = 0; $x -lt $this.map.Count; $x++) {
             for ($y = 0; $y -lt $this.map[$x].Count; $y++) {
 
-                $this.map[$x][$y] = [Ground]::new()
+                $this.map[$x][$y] = [Ground]::new($x,$y)
 
                 if( $x -eq 0 -or $y -eq 0 ){ 
-                    $this.map[$x][$y] = [Wall]::new()
+                    $this.map[$x][$y] = [Wall]::new($x,$y)
                 }
                 if( $x -eq $this.map.Count -1 -or $y -eq $this.map[$x].Count -1 ){ 
-                    $this.map[$x][$y] = [Wall]::new()
+                    $this.map[$x][$y] = [Wall]::new($x,$y)
                 }
             }
         }
@@ -47,7 +59,6 @@ Class Level{
     Show(){
         for ($x = 0; $x -lt $this.map.Count ; $x++) {
             for ($y = 0; $y -lt $this.map[$x].Count; $y++) {
-                [Console]::SetCursorPosition($x, $y)
                 $object = $this.map[$x][$y]
                 $object.Show()
             }
@@ -56,28 +67,57 @@ Class Level{
 }
 
 Class Ground{
+
     [char]$char = "."
+    [int]$x
+    [int]$y
+    [bool]$solid = $false
+
+    Ground($x, $y){
+        $this.x = $x
+        $this.y = $y
+    }
 
     Show(){
+        [Console]::SetCursorPosition($this.x, $this.y)
         [Console]::ForegroundColor = "DarkGray"
 	    [Console]::Write($this.char)
     }
 }
 
 Class Wall{
+
     [char]$char = "#"
+    [int]$x
+    [int]$y
+    [bool]$solid = $true
+
+    Wall($x, $y){
+        $this.x = $x
+        $this.y = $y
+    }
 
     Show(){
+        [Console]::SetCursorPosition($this.x, $this.y)
         [Console]::ForegroundColor = "Gray"
 	    [Console]::Write($this.char)
     }
 }
 
-Class Exit{
+Class Out{
 
     [char]$char = "D"
-    
+    [int]$x
+    [int]$y
+    [bool]$solid = $false
+
+    Out($x, $y){
+        $this.x = $x
+        $this.y = $y
+    }
+
     Show(){
+        [Console]::SetCursorPosition($this.x, $this.y)
         [Console]::ForegroundColor = "Red"
 	    [Console]::Write($this.char)
     }
@@ -86,49 +126,50 @@ Class Exit{
 Class Player{
 
     [char]$char = "@"
-    [Hashtable]$pos = @{ x = 0; y = 0}
+    [int]$x
+    [int]$y
+    [bool]$solid = $false
 
-    Player([int]$x,[int]$y,[char]$char){
-        $this.pos.x = $x
-        $this.pos.y = $y
-        $this.char = $char
+    Player([int]$x,[int]$y){
+        $this.x = $x
+        $this.y = $y
     }
 
     Show(){
-        [Console]::SetCursorPosition($this.pos.x, $this.pos.y)
+        [Console]::SetCursorPosition($this.x, $this.y)
         [Console]::ForegroundColor = "Cyan"
 	    [Console]::Write($this.char)
     }
 
-    MoveRight(){ $this.pos.x ++ }
-    MoveLeft() { $this.pos.x -- }
-    MoveDown() { $this.pos.y ++ }
-    MoveUP()   { $this.pos.y -- }   
+    MoveRight(){ $this.x ++ }
+    MoveLeft() { $this.x -- }
+    MoveDown() { $this.y ++ }
+    MoveUP()   { $this.y -- }   
 }
 
 function LevelGenerator(){
     
-    $L = [Level]::new(20,20)
-    $L.map[5][0] = [Exit]::new()
+    $L = [Level]::new($levelWidth, $levelHeight)
+    #$L.map[5][0] = [Out]::new(5,0)
     $step = 2
 
     for ($x = $step; $x -lt $L.map[$x].Count - $step; $x++) {
-            for ($y = $step; $y -lt $L.map[$x].Count - $step; $y++) {
-
-                $rnd = Get-Random -InputObject (0..10)
-                if( $rnd -eq 0){
-                    
-                    $L.map[$x][$y] = [Exit]::new()
-                }
+        for ($y = $step; $y -lt $L.map[$x].Count - $step; $y++) {
+            <#
+                Написать внятный рандом
+            #>
+            if((Get-Random $levelWidth) -eq (Get-Random $levelWidth)){
+                $L.map[$x][$y] = [Out]::new($x,$y)
             }
+        }
     }
     Return $L
 }
 
 function PlayerMovement($key){
     
-    $PPX = $player.pos.x
-    $PPY = $player.pos.y
+    $PPX = $player.x
+    $PPY = $player.y
 
     if ($key -eq "RightArrow"){ 
         if($level.map[$PPX +1][$PPY].char -eq "."){$player.MoveRight()}
@@ -145,7 +186,7 @@ function PlayerMovement($key){
 }
 
 $level = LevelGenerator
-$player = [Player]::new(1,1, "@")
+$player = [Player]::new(1,1)
 
 do{ # main loop
     [Console]::BackgroundColor = "DarkBlue"
